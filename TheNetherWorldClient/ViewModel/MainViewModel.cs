bf2ac34999f533a11e3cd135bcac845ba260001c
aliyunzzb <2969773896@qq.com>
2025-04-24 16:17:20 +0800

    退回，3个Task

--- TheNetherWorldClient/ViewModel/MainViewModel.cs
+++ TheNetherWorldClient/ViewModel/MainViewModel.cs
@@ -47,8 +47,9 @@ namespace TheNetherWorldClient.ViewModel
             get
             {
                 return new RelayCommand(() => {
-                    Task.Run(() =>{ GetRegistrationCount(); GetJudgmentInfoCount(); GetLogoutCount(); });
-                    
+                    Task.Run(() =>{ GetRegistrationCount(); });
+                    Task.Run(() =>{ GetJudgmentInfoCount(); });
+                    Task.Run(() =>{ GetLogoutCount(); });
 
                 });
             }
